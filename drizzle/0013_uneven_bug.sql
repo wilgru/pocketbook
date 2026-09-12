e22c477492afbd4194e45cc14a239614fbe948df
William Gruszka <40753609+wilgru@users.noreply.github.com>
2026-09-12 10:03:35 +1000

    fixed migration 13

--- drizzle/0013_uneven_bug.sql
+++ drizzle/0013_uneven_bug.sql
@@ -1,29 +1,4 @@
 PRAGMA foreign_keys=OFF;--> statement-breakpoint
-CREATE TABLE `__new_tasks` (
-	`id` text PRIMARY KEY NOT NULL,
-	`title` text DEFAULT '' NOT NULL,
-	`description` text DEFAULT '' NOT NULL,
-	`link` text,
-	`links` text DEFAULT '[]' NOT NULL,
-	`is_important` integer DEFAULT false NOT NULL,
-	`noteId` text,
-	`blocked_comment` text,
-	`due_date` text,
-	`completed_date` text,
-	`cancelled_date` text,
-	`blocked_date` text,
-	`sort_order` integer DEFAULT 0 NOT NULL,
-	`pocketbook` text NOT NULL,
-	`created` text NOT NULL,
-	`updated` text NOT NULL,
-	FOREIGN KEY (`noteId`) REFERENCES `notes`(`id`) ON UPDATE no action ON DELETE no action,
-	FOREIGN KEY (`pocketbook`) REFERENCES `pocketbooks`(`id`) ON UPDATE no action ON DELETE no action
-);
---> statement-breakpoint
-INSERT INTO `__new_tasks`("id", "title", "description", "link", "links", "is_important", "noteId", "blocked_comment", "due_date", "completed_date", "cancelled_date", "blocked_date", "sort_order", "pocketbook", "created", "updated") SELECT "id", "title", "description", "link", "links", "is_important", "noteId", "blocked_comment", "due_date", "completed_date", "cancelled_date", "blocked_date", "sort_order", "pocketbook", "created", "updated" FROM `tasks`;--> statement-breakpoint
-DROP TABLE `tasks`;--> statement-breakpoint
-ALTER TABLE `__new_tasks` RENAME TO `tasks`;--> statement-breakpoint
-PRAGMA foreign_keys=ON;--> statement-breakpoint
 CREATE TABLE `__new_comments` (
 	`id` text PRIMARY KEY NOT NULL,
 	`content` text DEFAULT '{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"","type":"text","version":1}],"direction":null,"format":"","indent":0,"textFormat":0,"textStyle":"","type":"paragraph","version":1}],"direction":null,"format":"","indent":0,"type":"root","version":1}}' NOT NULL,
@@ -38,6 +13,7 @@ CREATE TABLE `__new_comments` (
 INSERT INTO `__new_comments`("id", "content", "tint", "is_waypoint", "pocketbook", "created", "updated") SELECT "id", "content", "tint", "is_waypoint", "pocketbook", "created", "updated" FROM `comments`;--> statement-breakpoint
 DROP TABLE `comments`;--> statement-breakpoint
 ALTER TABLE `__new_comments` RENAME TO `comments`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
 CREATE TABLE `__new_notes` (
 	`id` text PRIMARY KEY NOT NULL,
 	`title` text,
@@ -78,5 +54,29 @@ CREATE TABLE `__new_tags` (
 INSERT INTO `__new_tags`("id", "name", "colour", "icon", "layout", "description", "group_by", "group_by_tag_group_id", "sort_by", "sort_direction", "links", "tag_group", "pocketbook", "created", "updated") SELECT "id", "name", "colour", "icon", "layout", "description", "group_by", "group_by_tag_group_id", "sort_by", "sort_direction", "links", "tag_group", "pocketbook", "created", "updated" FROM `tags`;--> statement-breakpoint
 DROP TABLE `tags`;--> statement-breakpoint
 ALTER TABLE `__new_tags` RENAME TO `tags`;--> statement-breakpoint
+CREATE TABLE `__new_tasks` (
+	`id` text PRIMARY KEY NOT NULL,
+	`title` text DEFAULT '' NOT NULL,
+	`description` text DEFAULT '' NOT NULL,
+	`link` text,
+	`links` text DEFAULT '[]' NOT NULL,
+	`is_important` integer DEFAULT false NOT NULL,
+	`note` text,
+	`blocked_comment` text,
+	`due_date` text,
+	`completed_date` text,
+	`cancelled_date` text,
+	`blocked_date` text,
+	`sort_order` integer DEFAULT 0 NOT NULL,
+	`pocketbook` text NOT NULL,
+	`created` text NOT NULL,
+	`updated` text NOT NULL,
+	FOREIGN KEY (`note`) REFERENCES `notes`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`pocketbook`) REFERENCES `pocketbooks`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+INSERT INTO `__new_tasks`("id", "title", "description", "link", "links", "is_important", "note", "blocked_comment", "due_date", "completed_date", "cancelled_date", "blocked_date", "sort_order", "pocketbook", "created", "updated") SELECT "id", "title", "description", "link", "links", "is_important", "note", "blocked_comment", "due_date", "completed_date", "cancelled_date", "blocked_date", "sort_order", "pocketbook", "created", "updated" FROM `tasks`;--> statement-breakpoint
+DROP TABLE `tasks`;--> statement-breakpoint
+ALTER TABLE `__new_tasks` RENAME TO `tasks`;--> statement-breakpoint
 ALTER TABLE `pocketbooks` DROP COLUMN `user`;--> statement-breakpoint
 ALTER TABLE `tag_groups` DROP COLUMN `user`;
